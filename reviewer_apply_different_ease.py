48856a8309294b93161a19550a3de4fa84d6adba
ijgnd <40218852+ijgnd@users.noreply.github.com>
2019-02-02 16:27:11 +0100

    Update reviewer_apply_different_ease.py

--- reviewer_apply_different_ease.py
+++ reviewer_apply_different_ease.py
@@ -3,23 +3,24 @@ from __future__ import unicode_literals
 
 
 """
+anki-addon copyright 2018 ijgnord
+
+This add-on modifies some functions of anki/sched.py which is
+     Copyright: Damien Elmes <anki@ichi2.net>
+
+
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-some code reused from anki - anki/sched.py which is
-     Copyright: Damien Elmes <anki@ichi2.net>
-
-changes: copyright 2018 ijgnord
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 # To view the ease value of the current and prior card during reviews use the add-on
